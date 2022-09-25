778ae589b0269520876197ada30619caf949fa58
Majeed <rahmanmajeed46@gmail.com>
2022-09-26 00:41:52 +0600

    [init] auto-typewriter package.

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+Author: Majeed
+Date: 26-Sep-22
+*/
+
+
+/**
+ * check el is HTMLElement.
+ * @param {*} el 
+ * @returns {boolean}
+ */
+export function isElement(el){
+    return el instanceof HTMLElement;
+}
